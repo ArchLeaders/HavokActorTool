7a123c2634e710b4682067d1259e262c7f0f36ca
ArchLeaders <archleadership28@gmail.com>
2024-10-22 15:48:01 -0700

    Add app icon

--- src/HavokActorTool/Views/ShellView.axaml.cs
+++ src/HavokActorTool/Views/ShellView.axaml.cs
@@ -1,3 +1,6 @@
+using Avalonia;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
 using FluentAvalonia.UI.Windowing;
 
 namespace HavokActorTool.Views;
@@ -7,5 +10,8 @@ public partial class ShellView : AppWindow
     public ShellView()
     {
         InitializeComponent();
+        
+        Bitmap bitmap = new(AssetLoader.Open(new Uri("avares://HavokActorTool/Assets/icon.ico")));
+        Icon = bitmap.CreateScaledBitmap(new PixelSize(48, 48));
     }
 }
