7f260a046cc375cb3dd6b9c6db3f9938cf2cfabb
ArchLeaders <archleadership28@gmail.com>
2024-10-22 12:11:19 -0700

    Create LifeConditionBuilder

---
+++ src/HavokActorTool.Core/ActorParams/LifeConditionBuilder.cs
@@ -0,0 +1,47 @@
+using Nintendo.Aamp;
+
+namespace HavokActorTool.Core.ActorParams;
+
+public class LifeConditionBuilder()
+{
+    public static AampFile Build(float lifeConditionDistance, out string lifeConditionUser)
+    {
+        lifeConditionUser = $"Landmark{lifeConditionDistance}m";
+        
+        var result = AampFile.New(2);
+        result.RootNode.ParamObjects = [
+            new ParamObject {
+                HashString = "DisplayDistance",
+                ParamEntries = [
+                    new ParamEntry {
+                        HashString = "Item",
+                        Value = lifeConditionDistance,
+                        ParamType = ParamType.Float
+                    }
+                ]
+            },
+            new ParamObject {
+                HashString = "AutoDisplayDistanceAlgorithm",
+                ParamEntries = [
+                    new ParamEntry {
+                        HashString = "Item",
+                        Value = "Bounding.Y",
+                        ParamType = ParamType.StringRef
+                    }
+                ]
+            },
+            new ParamObject {
+                HashString = "YLimitAlgorithm",
+                ParamEntries = [
+                    new ParamEntry {
+                        HashString = "Item",
+                        Value = "NoLimit",
+                        ParamType = ParamType.StringRef
+                    }
+                ]
+            }
+        ];
+
+        return result;
+    }
+}
