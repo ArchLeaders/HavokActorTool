bf922f74de24aab5c59072969b6df434c676f6fe
ArchLeaders <archleadership28@gmail.com>
2024-10-22 12:57:08 -0700

    Implement CLI program

--- src/HavokActorTool/Program.cs
+++ src/HavokActorTool/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello World!");
+﻿using System.Reflection;
+using ConsoleAppFramework;
+using HavokActorTool;
+using Kokuban;
+
+Console.WriteLine(Chalk.Bold.Gray +
+                  $"Havok Actor Tool [Version {Assembly.GetExecutingAssembly().GetName().Version?.ToString(3) ?? "???"}]\n");
+Console.WriteLine(Chalk.Underline.Gray +
+                  "(c) 2024 ArchLeaders. MIT.\n");
+
+unsafe {
+    ConsoleApp.Run(args, &Commands.BuildCommand);
+}
