19617fe06f20060a8742243ab6cd6ab55805aa30
ArchLeaders <archleadership28@gmail.com>
2024-10-22 18:31:10 -0700

    Create Lite (CLI only) project for native AOT

---
+++ src/HavokActorTool.Lite/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using ConsoleAppFramework;
+using HavokActorTool;
+using Kokuban;
+
+Console.WriteLine(Chalk.Bold.Gray +
+                  $"Havok Actor Tool [Version {Assembly.GetExecutingAssembly().GetName().Version?.ToString(3) ?? "???"}]\n");
+Console.WriteLine(Chalk.Underline.Gray +
+                  "(c) 2024 ArchLeaders. MIT.\n");
+
+ConsoleApp.ConsoleAppBuilder app = ConsoleApp.Create();
+
+app.Add("", Commands.BuildCommand);
+app.Add("config", Commands.ConfigCommand);
+
+app.Run(args);
