0802b57fa9b9024f02055c0d481143af57c9fb79
ArchLeaders <archleadership28@gmail.com>
2024-10-22 17:27:55 -0700

    Add validation attributes to HkActor

--- src/HavokActorTool.Core/HkActor.cs
+++ src/HavokActorTool.Core/HkActor.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace HavokActorTool.Core;
@@ -10,16 +11,19 @@ public sealed partial class HkActor(
     string? baseActorName = null,
     bool useCustomModel = false,
     float lifeCondition = 500)
-    : ObservableObject
+    : ObservableValidator
 {
     [ObservableProperty]
+    [Required(ErrorMessage = "Required field.")]
     private string _hkrbFilePath = hkrbFilePath;
 
     [ObservableProperty]
-    private string _name = name;
+    [Required(ErrorMessage = "Required field.")]
+    private string _outputModFolder = outputModFolder;
 
     [ObservableProperty]
-    private string _outputModFolder = outputModFolder;
+    [Required(ErrorMessage = "Required field.")]
+    private string _name = name;
 
     [ObservableProperty]
     private string _modelName = modelName ?? name;
@@ -31,9 +35,21 @@ public sealed partial class HkActor(
     private bool _useCustomModel = useCustomModel;
 
     [ObservableProperty]
+    [Required, Range(100, 100_000)]
     private float _lifeCondition = lifeCondition;
 
     public HkActor() : this(string.Empty, string.Empty, string.Empty)
     {
+    }
+
+    partial void OnNameChanged(string value)
+    {
+        if (!string.IsNullOrWhiteSpace(ModelName)) {
+            return;
+        }
+
+        ModelName = value.Split('_').LastOrDefault() is string lastNameArg && int.TryParse(lastNameArg, out _)
+            ? value[..^(lastNameArg.Length + 1)]
+            : value;
     }
 }
